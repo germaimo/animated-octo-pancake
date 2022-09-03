c02c3f5d3ab8bd339338f4ebd3faaf4efd140850
batimovil <german.figueroa@ort.edu.ar>
2022-09-03 00:31:51 -0300

    components folder added

--- pages/cardFeatures.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const cardFeatures = () => {
-  return (
-    <div>cardFeatures</div>
-  )
-}
-
-export default cardFeatures
--- pages/itemBenefits.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const itemBenefits = () => {
-  return (
-    <div>itemBenefits</div>
-  )
-}
-
-export default itemBenefits
